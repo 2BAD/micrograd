80b7200f3300b973ac056e9e81de554f556200cd
Jason Hyde <github@2bad.me>
2025-05-02 06:38:47 +0200

    [ts] add entrypoint

---
+++ source/index.ts
@@ -0,0 +1,4 @@
+// biome-ignore lint/performance/noBarrelFile:
+export { Layer, MLP, Neuron } from './neuron.ts'
+export { GraphVisualizer } from './utils/visualizer.ts'
+export { Value } from './value.ts'
